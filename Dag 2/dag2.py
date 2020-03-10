14f6385a00048f33499b00568c8a48a1a7f5bb3d
Lause <laura.bjurner@gmail.com>
2020-03-10 14:35:20 +0100

    Exercises and tests

---
+++ Dag 2/dag2.py
@@ -0,0 +1,53 @@
+fruit = "apple"
+print (len(fruit))
+fruit = "apple"
+print (fruit.upper())
+
+print ("I" + " Love" + " The" + " Jonas" + " Brothers")
+
+print ("Hello")
+print (" Hello you")
+
+print (" JoeBros" * 8)
+
+version = " Jonas"
+print ("I Love Joe" + version + ".")
+
+print ("{} {} {}".format ("I", "Love", "JoeBros"))
+
+
+#Exercise 1
+bird = " cat"
+worstfood = " broccoli"
+computer = " gold"
+
+print ("Here is a" + bird + ", a" + worstfood + ", and a" + computer + ".")
+
+#Exercise 2
+'''
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ("Hello there!")
+'Hello there!'
+>>> 
+ '''
+
+
+
+#Exercise 3
+print("                   _______________________")
+print("                 < Pet me and I will purr. >")
+print("                   -----------------------")
+print("                 /                 ­­­­­­­­­­­­­­­­­­­­­­­       ")
+print("        /\_/\   /                         ")
+print("       ( o.o )                            ")
+print("       >  ^  <                            ")
+
+
+
+
+
+
+
+
+
+
---
+++ Dag 2/test.py
@@ -0,0 +1,12 @@
+print("     /|" )
+print("    / |")
+print("   /  | ")
+print("  /___|")
+
+
+print("Laura sagde: \"Det her er svært\" ")
+
+
+besked = "Pet me and I will purr."
+print(" " * 2 + "_" * 24  )
+print("< " + besked + " >") 
