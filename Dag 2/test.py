080ec709f99105042978fd5a22031077abde53df
Lause <laura.bjurner@gmail.com>
2020-03-10 14:39:10 +0100

    jeg testede "len"

--- Dag 2/test.py
+++ Dag 2/test.py
@@ -7,6 +7,6 @@ print("  /___|")
 print("Laura sagde: \"Det her er svært\" ")
 
 
-besked = "Pet me and I will purr."
-print(" " * 2 + "_" * 24  )
+besked = "Pet me and I will purr im a sucker for you."
+print(" " * 2 + "_" * len (besked))
 print("< " + besked + " >") 
