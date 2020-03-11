ccca590736fb82d50662941b1ee644ff018aabe3
Lause <laura.bjurner@gmail.com>
2020-03-11 11:40:49 +0100

    Creating SpaceWar pt 5

--- Dag 2/test.py
+++ Dag 2/test.py
@@ -7,6 +7,8 @@ print("  /___|")
 print("Laura sagde: \"Det her er svært\" ")
 
 
-besked = "Pet me and I will purr im a sucker for you."
+besked = "Pet me and I will purr."
 print(" " * 2 + "_" * len (besked))
 print("< " + besked + " >") 
+
+udskriv("hey")
--- SpaceWar/Main.py
+++ SpaceWar/Main.py
@@ -72,6 +72,33 @@ class Enemy(Sprite):
         self.speed = 6
         self.setheading (random.randint(0,360))
 
+class Missile(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline = None)
+        self.speed = 20
+        self.status = "ready"
+        self.goto(-1000, 1000)
+
+    def fire(self):
+        if self.status == "ready":
+            self.goto(player.xcor(), player.ycor())
+            self.setheading(player.heading())
+            self.status = "firing"
+
+    def move(self):
+        
+        if self.status == "ready":
+            self.goto(-1000, 1000)
+        
+        if self.status == "firing":
+            self.fd(self.speed)
+
+        if self.xcor() < -290 or self.xcor() > 290 or \
+            self.ycor()< -290 or self.ycor()> 290:
+            self.goto(-1000, 1000)
+            self.status = "ready"
+
 class Game():
     def __init__(self):
         self.level = 1
@@ -100,23 +127,30 @@ game.draw_border()
 
 player = Player("triangle", "white", 0, 0)
 enemy = Enemy("circle", "red", -100, 0)
+missile = Missile("triangle", "yellow", 0, 0)
 
 turtle.onkey(player.turn_left, "Left")
 turtle.onkey(player.turn_right, "Right")
 turtle.onkey(player.accelerate, "Up")
 turtle.onkey(player.decelerate, "Down")
-
+turtle.onkey(missile.fire, "space")
 turtle.listen()
+
 while True:
     player.move()
     enemy.move()
+    missile.move()
 
     if player.is_collision(enemy):
        x = random.randint(-250, 250)
        y = random.randint(-250, 250)
        enemy.goto(x, y)
     
-
+    if missile.is_collision(enemy):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        enemy.goto(x, y)
+        missile.status = "ready"
 
 
 delay = input("Press enter to finish. >")
