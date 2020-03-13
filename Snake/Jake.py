6284a3eb5024cf51581b79da3d2fca409fb96d1d
Lause <laura.bjurner@gmail.com>
2020-03-13 11:22:09 +0100

    Creating Snake pt 4

--- Snake/Jake.py
+++ Snake/Jake.py
@@ -28,6 +28,7 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 
+segments = []
 
 
 #Functions
@@ -75,13 +76,32 @@ wn.onkeypress(go_right, "d")
 while True:
     wn.update()
 
+    #Check for a collision with the food
     if head.distance(food) < 20:
         # Move the food to a random spot
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x, y)
 
+        #Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("purple")
+        new_segment.penup()
+        segments.append(new_segment)
 
+    # Move the end segments first in revers order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
 
     move()
 
