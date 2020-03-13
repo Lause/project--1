60871df802371a77852e319a6cf9896382a29eb6
Lause <laura.bjurner@gmail.com>
2020-03-13 10:32:12 +0100

    Creating Snake pt 1

---
+++ Snake/Jake.py
@@ -0,0 +1,13 @@
+import turtle
+
+#set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game By Lause")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+
+
+wn.mainloop()
